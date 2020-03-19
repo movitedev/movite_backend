2828ec068112d27a48b10870eb2cebaed07e384e
movitedev <movitedev@gmail.com>
2020-03-19 22:09:36 +0100

    Structure

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: 'mongodb+srv://admin:kbGIYyXz0G2z8VSQ@cluster0-srp04.mongodb.net/test?retryWrites=true&w=majority',
+    serverport: 8080 
+}
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:kbGIYyXz0G2z8VSQ@cluster0-srp04.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));// Launch app to listen to specified port
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let app = express();
+
+const config = require('./config.js');
+let apiRoutes = require('./api-routes');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+mongoose.connect(config.url, { useNewUrlParser: true,  useUnifiedTopology: true,});
+var db = mongoose.connection;
+
+if(!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
+
+var port = process.env.PORT || config.serverport;
+
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+app.use('/api', apiRoutes);
 app.listen(port, function () {
-     console.log("Running RestHub on port " + port);
-});
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+    console.log("Running ApiServer on port " + port);
 });
